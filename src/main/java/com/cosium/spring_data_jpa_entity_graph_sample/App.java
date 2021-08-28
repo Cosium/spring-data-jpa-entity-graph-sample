48d8b2b0a623eaa964b425d35086e4d73d2327b2
Réda Housni Alaoui <reda-alaoui@hey.com>
2021-08-28 21:00:22 +0200

    Spring Boot project initialization

---
+++ src/main/java/com/cosium/spring_data_jpa_entity_graph_sample/App.java
@@ -0,0 +1,13 @@
+package com.cosium.spring_data_jpa_entity_graph_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
