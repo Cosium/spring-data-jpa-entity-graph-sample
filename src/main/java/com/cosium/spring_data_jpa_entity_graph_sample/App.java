4f63799bca96fa3d89b98b8cf5af6d0cc6f50b87
Réda Housni Alaoui <reda-alaoui@hey.com>
2021-08-29 18:04:51 +0200

    Add product endpoint and README

--- src/main/java/com/cosium/spring_data_jpa_entity_graph_sample/App.java
+++ src/main/java/com/cosium/spring_data_jpa_entity_graph_sample/App.java
@@ -1,9 +1,12 @@
 package com.cosium.spring_data_jpa_entity_graph_sample;
 
+import com.cosium.spring.data.jpa.entity.graph.repository.support.EntityGraphJpaRepositoryFactoryBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EntityGraphJpaRepositoryFactoryBean.class)
 public class App {
 
 	public static void main(String[] args) {
